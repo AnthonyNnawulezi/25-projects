92830398dc40be89ad7379bcaede100a9b45813d
Anthony <evansanton58@gmail.com>
2026-05-19 20:45:16 +0100

    finlaised

--- 1.25-projects/src/components/quote-generator/index.jsx
+++ 1.25-projects/src/components/quote-generator/index.jsx
@@ -1,51 +1,51 @@
-// import { useEffect } from "react";
-// import { useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./style.css";
-// // ""https://api.quotable.io/quotes/random"
-// //https://api/quotes/random
 
-// function RandonQuoteGenerator() {
-//   const [loading, setLoading] = useState(false);
-//   const [quote, setQuote] = useState(null);
+function RandonQuoteGenerator() {
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState(null);
 
-//   async function fetchQuote() {
-//     try {
-//       setLoading(true);
+  async function fetchQuote() {
+    try {
+      setLoading(true);
 
-//       const response = await fetch("https://api.quotable.io/quotes/random", {
-//         method: "GET",
-//       });
-//       const result = await response.json();
-//       console.log(result);
-//       if (result && result.length > 0) {
-//         setLoading(false);
-//         setQuote(result[0]);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   useEffect(() => {
-//     fetchQuote();
-//   }, []);
+      const response = await fetch("https://dummyjson.com/quotes/random");
+      const result = await response.json();
 
-//   function handleRefresh() {
-//     fetchQuote();
-//   }
-//   if (loading) {
-//     return <h3>Loading Quote! please wait</h3>;
-//   }
+      if (result && result.quote) {
+        setQuote(result);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-//   return (
-//     <div className="random-quote-generator">
-//       <h1>Randon Quote Generator</h1>
-//       <div className="wrapper">
-//         <p>{quote?.author}</p>
-//         <p>{quote?.content}</p>
-//         <button className="refresh" onClick={handleRefresh}>Refresh</button>
-//       </div>
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
-// export default RandonQuoteGenerator;
+  function handleRefresh() {
+    fetchQuote();
+  }
+
+  if (loading) {
+    return <h3>Loading Quote! please wait</h3>;
+  }
+
+  return (
+    <div className="random-quote-generator">
+      <h1>Randon Quote Generator</h1>
+      <div className="wrapper">
+        <p>{quote?.quote}</p>
+        <p>{quote?.author}</p>
+        <button className="refresh" onClick={handleRefresh}>
+          Refresh
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default RandonQuoteGenerator;
