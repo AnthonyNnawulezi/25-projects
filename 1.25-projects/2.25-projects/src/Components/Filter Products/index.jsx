fce1a3b323cef17357cccfcbfa0343aa722c6920
Anthony <evansanton58@gmail.com>
2026-05-26 21:04:59 +0100

    tolowercase bugfix

--- 1.25-projects/2.25-projects/src/Components/Filter Products/index.jsx
+++ 1.25-projects/2.25-projects/src/Components/Filter Products/index.jsx
@@ -1,11 +1,11 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import './style.css'
+import "./style.css";
 
 function FilterProducts() {
   const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [error, setError] = useState("");
-    const [selectedCategory, setSelectedCategory] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(false);
 
   const fetchProducts = useCallback(async () => {
     setError("");
@@ -17,13 +17,12 @@ function FilterProducts() {
       if (!response.ok) throw new Error("Error fetching Products");
 
       const data = await response.json();
-      console.log(data);
 
       setProducts(data.products ?? []);
     } catch (error) {
-        <p>{'Error fetching Products', error}</p>
-    }finally{
-        setIsLoading(false)
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }, []);
 
@@ -32,44 +31,48 @@ function FilterProducts() {
   }, []);
 
   const categories = useMemo(() => {
-    return [...new Set(products.map((product) => product.category))]
-  }, [products])
-  
+    return [...new Set(products.map((product) => product.category))];
+  }, [products]);
 
   const filteredProducts = useMemo(() => {
-    if(!selectedCategory) return products;
-    
-  console.log(selectedCategory);
-    
-    return products.map((product) => product.category.toLowerCase === selectedCategory.toLowerCase)
-  }, [products, selectedCategory])
+    if (!selectedCategory) return products;
 
+    return products.filter(
+      (product) =>
+        product.category.toLowerCase() === selectedCategory.toLowerCase(),
+    );
+  }, [products, selectedCategory]);
 
   return (
     <section className="filter-products-container">
       <h1>Filter Products By Category</h1>
       {isLoading && <p>Loading Please wait...</p>}
       {error && <p>Error Fetching Products</p>}
-     <div className="filter-button-wrapper">
-         {
-        categories.map((category) => (
-            <button key={category} type="button" onClick={() => setSelectedCategory((prev) => prev === category ? "" : category)} className={`filter-button ${selectedCategory === category ? 'active' : ""}`} >{category}</button>
-        ))
-      }
-     </div>
+      <div className="filter-button-wrapper">
+        {categories.map((category) => (
+          <button
+            key={category}
+            type="button"
+            onClick={() =>
+              setSelectedCategory((prev) => (prev === category ? "" : category))
+            }
+            className={`filter-button ${selectedCategory === category ? "active" : ""}`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
 
-<ul  className="filter-products-list">
-      {
-        filteredProducts.map(({id, category, images, brand, title}) => (
-                <li className="filter-list" key={id}>
-                    <img src={images} alt="" />
-                    <h3>{title}</h3>
-                    <h4>{category}</h4>
-                    <h5>Brand:{brand}</h5>
-                </li>
-        ))
-    }
-    </ul>
+      <ul className="filter-products-list">
+        {filteredProducts.map(({ id, category, images, brand, title }) => (
+          <li className="filter-list" key={id}>
+            <img src={images} alt="" />
+            <h3>{title}</h3>
+            <h4>{category}</h4>
+            <h5>Brand:{brand}</h5>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
