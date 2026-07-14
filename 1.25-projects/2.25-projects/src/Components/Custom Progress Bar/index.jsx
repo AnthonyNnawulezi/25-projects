b5b530b649cd4e1359e8a5996bfdb64356a90731
antonsprime <evansanton58@gmail.com>
2026-07-14 19:37:10 +0100

    Validates new input, Correct logic that prevents invalid updates.

--- 1.25-projects/2.25-projects/src/Components/Custom Progress Bar/index.jsx
+++ 1.25-projects/2.25-projects/src/Components/Custom Progress Bar/index.jsx
@@ -1,1 +1,11 @@
 import "./style.css";
+
+function ProgressBar() {
+  const [progressPercentage, setProgressPercentage] = useState(0);
+
+  function handlePercentageChange(event) {
+    const value = Number(event.target.value);
+  }
+}
+
+export default ProgressBar;
