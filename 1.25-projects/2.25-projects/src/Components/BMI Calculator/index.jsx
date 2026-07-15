033a4176fc8e0b648811cc9c7d2975fe1bc78bd3
antonsprime <evansanton58@gmail.com>
2026-07-16 00:14:47 +0100

    Extracted the nested ternary into a small named function

--- 1.25-projects/2.25-projects/src/Components/BMI Calculator/index.jsx
+++ 1.25-projects/2.25-projects/src/Components/BMI Calculator/index.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./style.css";
 
-function BMICalculator() {}
+function BMICalculator() {
+  function getBMICategory(bmiValue) {
+    if (bmiValue < 18.5) return "You are underweight";
+    if (bmiValue < 25) return "You are normal";
+    if (bmiValue < 30) return "You are overweight";
+    return "You are obese";
+  }
+}
 
 export default BMICalculator;
