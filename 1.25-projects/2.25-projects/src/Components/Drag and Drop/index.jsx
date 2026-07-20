52422857b6ac28e3dd82a3f80b874431d5ae6e9b
antonsprime <evansanton58@gmail.com>
2026-07-20 20:12:08 +0100

    bug fix continue

--- 1.25-projects/2.25-projects/src/Components/Drag and Drop/index.jsx
+++ 1.25-projects/2.25-projects/src/Components/Drag and Drop/index.jsx
@@ -30,11 +30,40 @@ function DragAndDrop() {
     fetchTodos();
   }, []);
 
+  //   function completedTodo() {
+  //     for (let i = 0; i < todos.length; i++) {
+  //       const listOfTodos = todos[i].completed.true;
+
+  //       return listOfTodos;
+  //     }
+  //   }
+  //   function inProgressTodo() {
+  //     for (let i = 0; i < todos.length; i++) {
+  //       const listOfTodos = todos[i].completed.false;
+
+  //       return listOfTodos;
+  //     }
+  //   }
+
+  function onMouseOver(todo) {
+    const listOfTodos = [...todos, todos.push()];
+  }
+
   return (
     <div className="drag-and-drop-container">
       <h1>Drag And Drop</h1>
       <div className="drag-and-drop-wrapper">
-        <div className="todo-wrapper-">In Progress</div>
+        <div className="todo-wrapper-">
+          <p>In Progress</p>
+          {todos &&
+            todos.map((todo) => (
+              <ul onMouseOver={(todo) => onMouseOver(todo)} onDrop={onDrop}>
+                <li key={todo.id} draggable>
+                  {todo.todo}
+                </li>
+              </ul>
+            ))}
+        </div>
         <div className="todo-wrapper">Completed</div>
       </div>
     </div>
